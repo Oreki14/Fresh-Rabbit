9b868b88d6f52cc73fd03f3e15f41934d581e11a
Oreki <oreki1457@gmail.com>
2023-10-13 21:08:21 +0800

    安装axios

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import { getCategory } from '@/apis/testAPI'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+ // 测试接口函数
+getCategory().then(res => {
+    console.log(res)
+})
