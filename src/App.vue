b3eeccbbcd0edce3f306e58dfb27be426f4b7201
Oreki <oreki1457@gmail.com>
2023-10-13 20:08:19 +0800

    完成配色定制

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+/*import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'*/
 </script>
 
 <template>
-  <header>
+  <el-button type="primary">Primary</el-button>
+<!--  <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,7 +18,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView />-->
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,40 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// 配置ElementUIPlus按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        // 自动导入需要的plus包
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [
+                // 1.配置ElementPlus采用sass样式配色系统
+                ElementPlusResolver( { importStyle: "sass" } )
+            ],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@':
+                fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // 2.自动导入定制化样式文件进行样式覆盖
+                additionalData: `
+                @use "@/styles/element/index.scss" as *;
+                `,
+            }
+        }
     }
-  }
 })
