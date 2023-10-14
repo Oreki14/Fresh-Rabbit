cbcf973509ce6d414db8937f3940547a092f84ee
Oreki <oreki1457@gmail.com>
2023-10-14 15:35:56 +0800

    scss文件自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,16 @@
 <template>
 <!--  一级路由出口组件  -->
   <RouterView/>
+<!-- 测试颜色 -->
+  <div class="test">
+    test scss
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test {
+  color: $priceColor;
+}
 header {
   line-height: 1.5;
   max-height: 100vh;
