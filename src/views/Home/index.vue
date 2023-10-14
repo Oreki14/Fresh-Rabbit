2eed96d5da5d8a7340ecbbfff12fc69707b5f2c2
Oreki <oreki1457@gmail.com>
2023-10-14 17:14:08 +0800

    首页初步构建

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,9 +3,6 @@
 </script>
 
 <template>
-  <div>
-    <h1>我是Home页</h1>
-  </div>
 </template>
 
 <style scoped>
--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,17 @@
 <script setup>
+import {getCategoryAPI} from '@/apis/layout'
+import {onMounted, ref} from 'vue'
+// 使用在api中定义的函数拉取nav数据
 
+const categoryList = ref([]) // 准备响应式数据
+const getCategory = async () => {
+  const res = await getCategoryAPI()
+  console.log(res)
+  categoryList.value = res.result
+}
+onMounted(() => {
+  getCategory()
+})
 </script>
 
 <template>
@@ -9,12 +21,10 @@
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
+        <!-- 使用v-for遍历数据 -->
+        <li class="home" v-for="item in categoryList" :key="item.id">
+          <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
-        <li> <RouterLink to="/">居家</RouterLink> </li>
-        <li> <RouterLink to="/">美食</RouterLink> </li>
-        <li> <RouterLink to="/">服饰</RouterLink> </li>
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>
