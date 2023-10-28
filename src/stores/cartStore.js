fc67a076945dc49693c17c8fb50387ea1b8fdc14
Oreki <oreki1457@gmail.com>
2023-10-28 14:57:39 +0800

    本地购物车

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,7 +1,8 @@
 // 封装购物车模块
 import {defineStore} from 'pinia'
-import {ref} from 'vue';
-export const useCartStore = defineStore('cart',() => {
+import {ref,computed} from 'vue';
+
+export const useCartStore = defineStore('cart', () => {
     // 1.定义state - cartList
     const cartList = ref([])
     // 2.定义action - addCart
@@ -14,15 +15,32 @@ export const useCartStore = defineStore('cart',() => {
         if (item) {
             // 找到了
             item.count++
-        }else {
+        } else {
             // 没找到则push
             cartList.value.push(goods)
         }
     }
+    // 删除购物车
+    const delCart = (skuId) => {
+        //
+        const idx = cartList.value.findIndex((item)=>skuId === item.skuId)
+        cartList.value.splice(idx, 1)
+    }
+    /*
+    计算属性
+     */
+    // 1.总的数量
+    const allCount = computed(() => cartList.value.reduce((a,c) => a + c.count,0))
+    // 2.总的价格
+    const allPrice = computed(() => cartList.value.reduce((a,c) => a + c.price * c.count,0))
+
     return {
         cartList,
-        addCart
+        addCart,
+        delCart,
+        allCount,
+        allPrice
     }
-},{
+}, {
     persist: true
 })
--- src/views/Layout/components/HeaderCart.vue
+++ src/views/Layout/components/HeaderCart.vue
@@ -25,13 +25,13 @@ const cartStore = useCartStore()
               <p class="count">x{{ i.count }}</p>
             </div>
           </RouterLink>
-          <i class="iconfont icon-close-new" @click="store.delCart(i.skuId)"></i>
+          <i class="iconfont icon-close-new" @click="cartStore.delCart(i.skuId)"></i>
         </div>
       </div>
       <div class="foot">
         <div class="total">
-          <p>共 {{ cartStore.cartList.length }} 件商品</p>
-          <p>&yen; 100.00 </p>
+          <p>共 {{ cartStore.allCount }} 件商品</p>
+          <p>&yen; {{ cartStore.allPrice.toFixed(2) }} </p>
         </div>
         <el-button size="large" type="primary" >去购物车结算</el-button>
       </div>
